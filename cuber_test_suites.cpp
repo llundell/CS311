f42007e2e6619ae88a0b5ad8f10d24249b969e63
llundell <llundell@ymail.com>
2018-11-29 14:27:31 -0900

    adding all initial files for Project 8

---
+++ cuber_test_suites.cpp
@@ -0,0 +1,36 @@
+//	cuber_test_suites.cpp
+//	followers.cpp
+//	Laura Lundell & Khan Howe
+//	For CS 311 Fall 2018
+//	Started: 11/29/18
+//	Updated: 12/4/18
+//	Source for cuber_test_suites.cpp
+//	Tests for class Cuber: test suites
+//	Exercise B: Writing a Test Suite
+//	Uses the "Catch" unit-testing framework, version 2
+//	Requires cuber_test_main.cpp, catch.hpp, cuber.h
+/*	Sources used:
+*			Dr. Chappell's slides on Project 8 thoughts and his skeleton file
+*		
+*/
+
+// Includes for code to be tested
+#include "cuber.h"         // For class Cuber
+#include "cuber.h"         // Double inclusion test
+
+#define CATCH_CONFIG_FAST_COMPILE
+                             // Disable some features for faster compile
+#include "catch.hpp"         // For the "Catch" unit-testing framework
+
+// Additional includes for this test program
+
+
+// *********************************************************************
+// Test Cases
+// *********************************************************************
+
+
+TEST_CASE("DUMMY")
+{
+    // DUMMY TEST CASE
+}
---
+++ followers.cpp
@@ -0,0 +1,10 @@
+//	followers.cpp
+//	Laura Lundell & Khan Howe
+//	Started: 11/29/18
+//	Updated: 12/4/18
+//	Source for followers.cpp
+//	Exercise A: Following-Words Program
+/*	Sources used:
+*			Dr. Chappell's slides on Project 8 thoughts
+*
+*/
