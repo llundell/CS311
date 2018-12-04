c966ee6573dc453ccbb61fb3a7052258ba8039a8
Khan Overcast <alaska113@gmail.com>
2018-12-03 19:19:05 -0900

    Part A finished, should clean up and seperate things into smaller fucntions a bit more.

--- followers.cpp
+++ followers.cpp
@@ -20,12 +20,53 @@ using std::endl;
 #include <fstream>
 using std::ifstream;
 #include <set>
+#include <map>
+#include <vector>
+#include <algorithm>    // std::sort
+
+void printWordMap(const std::map<string, std::vector<string> > & wordMap) {
+
+	cout << "Number of distinct words: " << wordMap.size() << endl;
+	for (auto iter = wordMap.cbegin(); iter != wordMap.cend(); iter++)
+	{
+		//retrieve vector, (so we can sort it. had issues sort via iter->secon.begin())
+		std::vector<string> v = iter->second;
+		//sort
+		std::sort(v.begin(),v.end());
+		//print the key
+	    cout << iter->first << ": ";
+
+		//loop vector, aka the value attached to the key
+	    for (auto list_iter = v.begin(); list_iter != v.end(); list_iter++)
+			//print each word to screen seperated by space
+	        cout << " " << *list_iter;
+		//move to new line
+		cout << endl;
+	}
+
+}
+
+bool currentWordIsNotPresentInKey(const std::vector<string> & wordVector, string word) {
+
+	//if find reaches the end of the vector witout finding the word, then it isnt yet present for that key
+	if (std::find(wordVector.begin(), wordVector.end(), word) == wordVector.end() ) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+
 
 int main()
 {
 	string file, word;
+	string previousWord;
 	ifstream inputFile;
 	std::set<string> wordList;
+	std::map<string, std::vector<string> > wordMap;
 
 	while (true)
 	{
@@ -39,10 +80,33 @@ int main()
 	int counts;
 	while(inputFile >> word)
 	{
+
+		//if were on the first word, just make the previous word the current word and move on
+		if (counts < 1) {
+			previousWord = word;
+			counts++;
+			continue;
+		}
+		else {
+			//if current word is not already in key, add it!
+			if (currentWordIsNotPresentInKey(wordMap[previousWord], word)) {
+				wordMap[previousWord].push_back(word);
+			}
+			//set previous word to current word
+			// previousWord = word;
+		}
+		previousWord = word;
 		counts++;
+
+		//TODO get rid of old testing
 		wordList.insert(word);
 	}
+	wordMap[previousWord].push_back("");
 
+
+	printWordMap(wordMap);
+
+	//TODO get rid of old testing
 	cout<< "There are " << counts << " total words and " <<
 		wordList.size() << " distinct words in the file." << endl;
 	return 0;
